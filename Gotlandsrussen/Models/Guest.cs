4060a6eb847f527a1a69ea41bcaafebcb6b34bcd
theMargarita <margotvoistinova@hotmail.se>
2025-05-13 16:08:24 +0200

    Created Guest model

---
+++ Gotlandsrussen/Models/Guest.cs
@@ -0,0 +1,18 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gotlandsrussen.Models
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+
+    }
+}
