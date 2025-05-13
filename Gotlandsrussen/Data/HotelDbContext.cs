5956e0456d13b81f324d7885ab8eedd238ccd2bc
theMargarita <margotvoistinova@hotmail.se>
2025-05-13 16:10:26 +0200

    Created dbcontext and migration

---
+++ Gotlandsrussen/Data/HotelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Gotlandsrussen.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gotlandsrussen.Data
+{
+    public class HotelDbContext : DbContext
+    {
+        //private static IConfiguration _configuration = new ConfigurationBuilder()
+        //    .SetBasePath(Directory.GetCurrentDirectory())
+        //    .AddJsonFile("appsettings.json")
+        //    .Build();
+
+        public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options) { }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+
+    }
+}
