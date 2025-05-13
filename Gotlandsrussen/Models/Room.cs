095df831c244802fb85a6d239fff3361492e5f70
theMargarita <margotvoistinova@hotmail.se>
2025-05-13 16:08:48 +0200

    Created Room model

---
+++ Gotlandsrussen/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿namespace Gotlandsrussen.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; } = string.Empty;
+        public decimal PricePerNight { get; set; }
+
+        //Navigation properties
+        public RoomType RoomType { get; set; }
+        public ICollection<Booking> Booking { get; set; }
+    }
+    
+    //public enum RoomTypes
+    //{
+    //    Singel = 1,
+    //    Double = 2,
+    //    Suite = 4,
+    //    Family = 6,
+    //    HoneymoonSuite = 1//heart shaped bed
+    //}
+
+}
