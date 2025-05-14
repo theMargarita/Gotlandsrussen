691086fb500a17fb3e2ebf7ad1e55a87431e1201
Kim Andersson <Kimpabooy@live.com>
2025-05-14 20:54:03 +0200

    added BookingRoom model

---
+++ Gotlandsrussen/Models/BookingRoom.cs
@@ -0,0 +1,11 @@
+﻿namespace Gotlandsrussen.Models
+{
+    public class BookingRoom
+    {
+        public int BookingId { get; set; }
+        public Booking Booking { get; set; }
+
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+    }
+}
