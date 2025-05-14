dd22937ab55561dceb9ac90f71c1ccde6e79e42c
Kim Andersson <Kimpabooy@live.com>
2025-05-14 20:58:59 +0200

    changed Booking modell

--- Gotlandsrussen/Models/Booking.cs
+++ Gotlandsrussen/Models/Booking.cs
@@ -8,11 +8,11 @@
         public DateOnly BookedToDate { get; set; } // Booked room To this date
         public int NumberOfAdults { get; set; }
         public int NumberOfChildren { get; set; }
-        public bool Breakfast { get; set; }
         public bool BookingIsCancelled { get; set; } = false; // if gest wants to cancel the booking. 
+        public BreakfastOption? BreakfastOption { get; set; }
 
         //Nagivation properties
-        public Guest Guest { get; set; } 
-        public ICollection<Room> Room { get; set; }
+        public Guest Guest { get; set; }
+        public ICollection<BookingRoom> BookingRooms { get; set; }
     }
 }
