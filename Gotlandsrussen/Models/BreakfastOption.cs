009c5223d1542fe19a21434f511ebfb87d22b3b0
Kim Andersson <Kimpabooy@live.com>
2025-05-14 21:01:14 +0200

    added BreakfastOption class

---
+++ Gotlandsrussen/Models/BreakfastOption.cs
@@ -0,0 +1,12 @@
+﻿namespace Gotlandsrussen.Models
+{
+    public class BreakfastOption
+    {
+        public int Id { get; set; }
+        public int BookingId { get; set; }
+        public bool IsIncluded { get; set; } = false;
+
+        // Navigation
+        public Booking Booking { get; set; }
+    }
+}
