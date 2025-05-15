aaa66d6b515aa161624f045fecad58a5aaa243ee
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-15 11:18:46 +0200

    Changed ServiceName to Name

--- Gotlandsrussen/Models/Service.cs
+++ Gotlandsrussen/Models/Service.cs
@@ -3,7 +3,7 @@
     public class Service
     {
         public int Id { get; set; }
-        public string ServiceName { get; set; }
+        public string Name { get; set; }
         public decimal Price { get; set; }
         public ICollection<BookingService> BookingServices { get; set; }
     }
