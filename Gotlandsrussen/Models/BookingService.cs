2e5198d8797643880dd99b49b85f50ecf8a2a523
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-15 11:16:12 +0200

    added Id to class Bookingservice

--- Gotlandsrussen/Models/BookingService.cs
+++ Gotlandsrussen/Models/BookingService.cs
@@ -2,6 +2,7 @@
 {
     public class BookingService
     {
+        public int Id { get; set; }
         public int BookingId { get; set; }
         public Booking Booking { get; set; }
         public int ServiceId { get; set; }
