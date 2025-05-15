3bdf866ef8fb87304808413bbe036457f67b29ae
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-15 11:08:36 +0200

    New class bookingService

---
+++ Gotlandsrussen/Models/BookingService.cs
@@ -0,0 +1,11 @@
+﻿namespace Gotlandsrussen.Models
+{
+    public class BookingService
+    {
+        public int BookingId { get; set; }
+        public Booking Booking { get; set; }
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
+        public int Quantity { get; set; }
+    }
+}
