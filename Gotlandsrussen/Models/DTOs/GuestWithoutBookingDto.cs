9be9de5f6d7450ec1c600d14a166078fb8b6d7b0
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-16 13:45:45 +0200

    new dto: GuestWithoutBookingDto

---
+++ Gotlandsrussen/Models/DTOs/GuestWithoutBookingDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gotlandsrussen.Models.DTOs
+{
+    public class GuestWithoutBookingDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string Phone { get; set; }
+    }
+}
