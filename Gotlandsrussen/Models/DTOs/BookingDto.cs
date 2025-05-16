fb367882aecf486a69d8ad359366c963b15cf568
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-16 13:14:39 +0200

    Created BookingDto

---
+++ Gotlandsrussen/Models/DTOs/BookingDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class BookingDto
+    {
+        public int Id { get; set; }
+        public Guest Guest { get; set; }
+        public List<string> Rooms { get; set; }
+        public DateOnly BookedFromDate { get; set; } 
+        public DateOnly BookedToDate { get; set; } 
+        public int NumberOfAdults { get; set; }
+        public int NumberOfChildren { get; set; }
+        public bool BookingIsCancelled { get; set; } = false;
+    }
+}
