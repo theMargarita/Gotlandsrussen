6a96d3af67555991fb90ea72cafc1a54b4d46a0c
Kim Andersson <Kimpabooy@live.com>
2025-05-19 15:15:38 +0200

    added GuestController

--- Gotlandsrussen/Controllers/GuestController.cs
+++ Gotlandsrussen/Controllers/GuestController.cs
@@ -3,9 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Gotlandsrussen.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/AddBreakfast")]
     [ApiController]
     public class GuestController : ControllerBase
     {
+        [HttpPost( Name = "AddBreakfast")]
+        public IActionResult AddBreakfast([FromHeader] int bookingId)
+        {
+            return Ok(new { message = "Breakfast added successfully." });
+        }
     }
 }
