815be82abcd8a1e148d0397883ab5ebae31baa56
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-19 15:57:21 +0200

    Moved LinaController into ManagementController

--- Gotlandsrussen/Controllers/ManagementController.cs
+++ Gotlandsrussen/Controllers/ManagementController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Gotlandsrussen.Data;
+using Gotlandsrussen.Models.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gotlandsrussen.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/GetAllFutureBookings")]
     [ApiController]
     public class ManagementController : ControllerBase
     {
+        private readonly HotelDbContext _context;
+
+        public ManagementController(HotelDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetAllFutureBookings")]
+        public async Task<ActionResult<ICollection<BookingDto>>> GetAllFutureBookings()
+        {
+            return Ok(await _context.Bookings
+                //.Include(b => b.BookingRooms)
+                //.ThenInclude(br => br.Room)
+                //.Include(b => b.Guest)
+                .Where(b => b.BookedFromDate >= DateOnly.FromDateTime(DateTime.Today)
+                    && b.BookingIsCancelled == false)
+                .Select(b => new BookingDto
+                {
+                    Id = b.Id,
+                    GuestName = b.Guest.LastName + ", " + b.Guest.FirstName,
+                    RoomNames = b.BookingRooms.Select(br => br.Room.RoomName).ToList(),
+                    BookedFromDate = b.BookedFromDate,
+                    BookedToDate = b.BookedToDate,
+                    NumberOfAdults = b.NumberOfAdults,
+                    NumberOfChildren = b.NumberOfChildren,
+                }).ToListAsync());
+        }
     }
 }
