836f28cd6e5afbe6d97589f5f7e9ac13e9deeb17
theMargarita <margotvoistinova@hotmail.se>
2025-05-21 21:11:05 +0200

    test one with this dto

---
+++ Gotlandsrussen/Models/DTOs/BookingRoomDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class BookingRoomDTO
+    {
+        //public int Id { get; set; }
+        public DateOnly BookedFromDate { get; set; }
+        public DateOnly BookedToDate { get; set; }
+        public int NumberOfAdults { get; set; }
+        public int NumberOfChildren { get; set; }
+        public List<string> RoomNames { get; set; }
+        public List<int> NumberOfBed { get; set; }
+
+    }
+}
