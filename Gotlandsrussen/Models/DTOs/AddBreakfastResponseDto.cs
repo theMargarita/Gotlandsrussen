2827f85b2e06a5c41650159b9ccd5e75446c3a41
Kim Andersson <Kimpabooy@live.com>
2025-05-22 11:27:01 +0200

    added a Request- and ResponseDto to AddBreakfast

---
+++ Gotlandsrussen/Models/DTOs/AddBreakfastResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class AddBreakfastResponseDto
+    {
+        public int BookingId { get; set; }
+        public bool Breakfast { get; set; }
+        public string Message { get; set; }
+    }
+}
