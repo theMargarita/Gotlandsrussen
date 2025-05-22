51d269cb30124cee7dcb39032286434430747480
theMargarita <margotvoistinova@hotmail.se>
2025-05-22 11:42:39 +0200

    navigation dto to gettig the roomtype isch

---
+++ Gotlandsrussen/Models/DTOs/GetAvailableDateDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class GetAvailableDateDTO
+    {
+        //public int Id { get; set; }
+        public DateOnly BookedFromDate { get; set; }
+        public DateOnly BookedToDate { get; set; }
+        public int NumberOfAdults { get; set; }
+        public int NumberOfChildren { get; set; }
+        //public List<string> RoomNames { get; set; }
+        //public List<int> NumberOfBed { get; set; }
+
+        public ICollection<RoomDTO> Rooms { get; set; }
+
+    }
+}
---
+++ Gotlandsrussen/Models/DTOs/RoomDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class RoomDTO
+    {
+        public string Name { get; set; }
+        public int NumberOfBeds { get; set; }
+
+    }
+}
