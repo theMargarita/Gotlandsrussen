da011e5c4f769d270cc63b0184aa85155cb44288
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-22 15:33:09 +0200

    Created a RoomWithPriceDto

---
+++ Gotlandsrussen/Models/DTOs/RoomWithPriceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class RoomWithPriceDto
+    {
+        public string RoomType { get; set; } = string.Empty;
+        public decimal PricePerNight { get; set; }
+    }
+}
