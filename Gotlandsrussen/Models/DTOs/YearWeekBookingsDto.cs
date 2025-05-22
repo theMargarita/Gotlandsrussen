36682374663b90db7a60e4d1afd5843eb1604d29
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-22 09:42:25 +0200

    Moved method GetIsoYearAndWeek to Utilityfolder

--- Gotlandsrussen/Models/DTOs/YearWeekBookingsDto.cs
+++ Gotlandsrussen/Models/DTOs/YearWeekBookingsDto.cs
@@ -8,25 +8,4 @@ namespace Gotlandsrussen.Models.DTOs
         public int Week { get; set; }
         public List<BookingDto>? Bookings { get; set; }
     }
-
-
-
-    // OBS - denna ska ligga någon annanstans. Skapa mapp. Var?
-    public static class HelpMethods
-    {
-        public static (int Year, int Week) GetIsoYearAndWeek(this DateOnly date)
-        {
-            var dateTime = date.ToDateTime(TimeOnly.MinValue);
-            var culture = CultureInfo.GetCultureInfo("sv-SE");
-            var calendar = culture.Calendar;
-            var weekRule = culture.DateTimeFormat.CalendarWeekRule;
-            var firstDayOfWeek = culture.DateTimeFormat.FirstDayOfWeek;
-
-            int week = calendar.GetWeekOfYear(dateTime, weekRule, firstDayOfWeek);
-            int year = ISOWeek.GetYear(dateTime);
-
-            return (year, week);
-        }
-    }
-
 }
--- Gotlandsrussen/Utilities/DateMethods.cs
+++ Gotlandsrussen/Utilities/DateMethods.cs
@@ -1,6 +1,21 @@
-﻿namespace Gotlandsrussen.Utilities
+﻿using System.Globalization;
+
+namespace Gotlandsrussen.Utilities
 {
-    public class DateMethods
+    public static class DateMethods
     {
+        public static (int Year, int Week) GetIsoYearAndWeek(this DateOnly date)
+        {
+            var dateTime = date.ToDateTime(TimeOnly.MinValue);
+            var culture = CultureInfo.GetCultureInfo("sv-SE");
+            var calendar = culture.Calendar;
+            var weekRule = culture.DateTimeFormat.CalendarWeekRule;
+            var firstDayOfWeek = culture.DateTimeFormat.FirstDayOfWeek;
+
+            int week = calendar.GetWeekOfYear(dateTime, weekRule, firstDayOfWeek);
+            int year = ISOWeek.GetYear(dateTime);
+
+            return (year, week);
+        }
     }
 }
