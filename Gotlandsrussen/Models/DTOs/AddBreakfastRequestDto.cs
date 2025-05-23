aa494916d276ddf8da9a7c0dadd0af6f4befaf56
Kim Andersson <Kimpabooy@live.com>
2025-05-23 12:39:48 +0200

    added AddBreakfastRequestDto

---
+++ Gotlandsrussen/Models/DTOs/AddBreakfastRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class AddBreakfastRequestDto
+    {
+        public int BookingId { get; set; }
+        public bool Breakfast { get; set; } = false;
+    }
+}
