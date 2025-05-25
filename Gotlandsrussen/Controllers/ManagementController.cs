9673bbd83320107beaa0e924d3c3408df20e9c3a
theMargarita <margotvoistinova@hotmail.se>
2025-05-25 19:53:55 +0200

    uncommented an if statement to check is getDate is null

--- Gotlandsrussen/Controllers/ManagementController.cs
+++ Gotlandsrussen/Controllers/ManagementController.cs
@@ -35,33 +35,18 @@ namespace Gotlandsrussen.Controllers
         {
             return Ok(await _bookingRepository.GetBookingsGroupedByMonth());
         }
-        //private readonly HotelDbContext _context;
-
-        //public ManagementController(HotelDbContext context)
-        //{
-        //    _context = context;
-        //}
 
 
         //som receptionist vill jag kunna söka lediga rum baserat på datum och antal gäster
         [HttpGet("{fromDate}/{toDate}/{adults}/{children}", Name = "GetAvailableRoomByDateAndGuests")]
         public async Task<ActionResult<ICollection<RoomDTO>>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children)
         {
-            //int totalGuests = adults + children;
-
-            //get date for the toom type
-            //var getDate = await _context.Bookings.Where(b => b.BookedFromDate == fromDate && b.BookedToDate == toDate).SelectMany(b => b.BookingRooms.Select(br => br.Room.RoomType.Name))
-            //    .ToListAsync();
-
-            //var getAvailableDate = await _context.Rooms.FirstOrDefaultAsync(r => r.RoomType == )
-
-            var getDate = _bookingRepository.GetAvailableRoomByDateAndGuests(fromDate, toDate, adults, children);
+            var getDate = await _bookingRepository.GetAvailableRoomByDateAndGuests(fromDate, toDate, adults, children);
 
             if (getDate == null)
             {
                 return BadRequest(new { errorMessage = "Date incorrectly typed" });
             }
-
 
             var dateNow = DateOnly.FromDateTime(DateTime.Now);
 
@@ -74,14 +59,10 @@ namespace Gotlandsrussen.Controllers
                 return BadRequest(new { errorMessage = "Cannot book for the same day" });
             }
 
-            //avaible room counted with the amout of guests
-            //var availableRooms = await _bookingRepository.GetAvailableRoomByDateAndGuests().Rooms.Where(r => r.RoomType.NumberOfBeds >= totalGuests).GroupBy(r => r.RoomType).Select(g => g.Key).ToListAsync();
-
-            //if (availableRooms == null)
-            //{
-            //    return BadRequest(new { errorMessage = "Invalid or typo" });
-            //}
-
+            if (getDate == null)
+            {
+                return BadRequest(new { errorMessage = "Invalid or typo" });
+            } 
 
             return Ok(getDate);
         }
