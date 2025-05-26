cb842f127d8026cac8cf5167a16061135a9a7ac7
theMargarita <margotvoistinova@hotmail.se>
2025-05-26 10:51:00 +0200

    Removed GetAvailableDate

--- Gotlandsrussen/Repositories/BookingRepository.cs
+++ Gotlandsrussen/Repositories/BookingRepository.cs
@@ -44,24 +44,6 @@ namespace Gotlandsrussen.Repositories
         }
 
 
-        public async Task<ICollection<RoomDTO>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children)
-        {
-            var availableRooms = await _context.Rooms
-            .Where(r => r.RoomType.NumberOfBeds >= (adults + children))
-            .Where(r => !_context.BookingRooms
-                .Any(br => br.RoomId == r.Id &&
-                   br.Booking.BookedFromDate <= toDate &&
-                   br.Booking.BookedToDate >= fromDate))
-            .Select(r => new RoomDTO
-            {
-                RoomName = r.RoomName,
-                Name = r.RoomType.Name,
-                NumberOfBeds = r.RoomType.NumberOfBeds,
-            })
-            .ToListAsync();
-
-            return availableRooms;
-        }
 
         public Task<ActionResult<Booking>> AddBreakfast(int bookingId)
         {
