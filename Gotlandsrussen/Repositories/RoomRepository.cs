864b4279d2e27bcc9fea3a03d409c240df2754cb
theMargarita <margotvoistinova@hotmail.se>
2025-05-26 10:51:25 +0200

    Added GetAvailableRoomByDateAndGuests

--- Gotlandsrussen/Repositories/RoomRepository.cs
+++ Gotlandsrussen/Repositories/RoomRepository.cs
@@ -1,12 +1,29 @@
 ﻿using Gotlandsrussen.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gotlandsrussen.Repositories
 {
     public class RoomRepository : IRoomRepository
     {
-        public Task<ICollection<RoomDTO>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children)
+
+        public async Task<ICollection<RoomDTO>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children)
         {
-            throw new NotImplementedException();
+            var availableRooms = await _context.Rooms
+            .Where(r => r.RoomType.NumberOfBeds >= (adults + children))
+            .Where(r => !_context.BookingRooms
+                .Any(br => br.RoomId == r.Id &&
+                   br.Booking.BookedFromDate <= toDate &&
+                   br.Booking.BookedToDate >= fromDate))
+            .Select(r => new RoomDTO
+            {
+                RoomName = r.RoomName,
+                Name = r.RoomType.Name,
+                NumberOfBeds = r.RoomType.NumberOfBeds,
+            })
+            .ToListAsync();
+
+            return availableRooms;
         }
+
     }
 }
