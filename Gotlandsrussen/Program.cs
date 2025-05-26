3a3fc23ded8826e5cbf7fe20e39b93ee8cc5981b
theMargarita <margotvoistinova@hotmail.se>
2025-05-26 11:14:13 +0200

    service controller

--- Gotlandsrussen/Program.cs
+++ Gotlandsrussen/Program.cs
@@ -13,11 +13,12 @@ namespace Gotlandsrussen
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+
+            //builder.Services.AddControllers();
             builder.Services.AddControllers().AddJsonOptions(x =>
-       x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-
-            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
