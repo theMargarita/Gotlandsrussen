4629b0beb79698cd447e2372fb61515f8a41598e
theMargarita <margotvoistinova@hotmail.se>
2025-05-26 11:24:09 +0200

    it was deleted somehow

---
+++ Gotlandsrussen/Models/DTOs/RoomDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class RoomDto
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public string RoomTypeName { get; set; }
+        public int NumberOfBeds { get; set; }
+        public decimal PricePerNight { get; set; }
+    }
+}
