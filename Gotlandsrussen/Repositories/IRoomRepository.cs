df155f41b8fe120beb147d7137dcb12b7a41070d
theMargarita <margotvoistinova@hotmail.se>
2025-05-26 11:12:13 +0200

    changed name on dto

--- Gotlandsrussen/Repositories/IRoomRepository.cs
+++ Gotlandsrussen/Repositories/IRoomRepository.cs
@@ -4,6 +4,6 @@ namespace Gotlandsrussen.Repositories
 {
     public interface IRoomRepository
     {
-        public Task<ICollection<RoomDTO>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children);
+        public Task<ICollection<RoomDto>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children);
     }
 }
