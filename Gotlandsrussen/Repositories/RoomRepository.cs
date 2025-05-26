08f6308bcf94f47ded11ecbe5a1b8f4a34a06fb4
theMargarita <margotvoistinova@hotmail.se>
2025-05-26 10:49:34 +0200

    inherited the method

---
+++ Gotlandsrussen/Repositories/RoomRepository.cs
@@ -0,0 +1,12 @@
+﻿using Gotlandsrussen.Models.DTOs;
+
+namespace Gotlandsrussen.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+        public Task<ICollection<RoomDTO>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
