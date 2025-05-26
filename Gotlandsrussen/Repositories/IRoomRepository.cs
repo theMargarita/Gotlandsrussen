f6799065f8714cb781ecee47dde7e6c6d6cf4ca8
theMargarita <margotvoistinova@hotmail.se>
2025-05-26 10:49:18 +0200

    Added a method

--- Gotlandsrussen/Repositories/IRoomRepository.cs
+++ Gotlandsrussen/Repositories/IRoomRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace Gotlandsrussen.Repositories
+﻿using Gotlandsrussen.Models.DTOs;
+
+namespace Gotlandsrussen.Repositories
 {
     public interface IRoomRepository
     {
-
+        public Task<ICollection<RoomDTO>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children);
     }
 }
