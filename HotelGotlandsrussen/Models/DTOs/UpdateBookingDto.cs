e75656fc55dcb3b758361c5e008f06fee66e08b5
FlorentHajdari <florent.hajdari@hotmail.se>
2025-05-27 21:46:34 +0200

    Filled UpdateBookingDto

--- HotelGotlandsrussen/Models/DTOs/UpdateBookingDto.cs
+++ HotelGotlandsrussen/Models/DTOs/UpdateBookingDto.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace HotelGotlandsrussen.Models.DTOs
 {
-    internal class UpdateBookingDto
+    public class UpdateBookingDto
     {
+        public int Id { get; set; }
+        public DateOnly BookedFromDate { get; set; }
+        public DateOnly BookedToDate { get; set; }
+        public int NumberOfAdults { get; set; }
+        public int NumberOfChildren { get; set; }
+        public bool Breakfast { get; set; }
     }
 }
