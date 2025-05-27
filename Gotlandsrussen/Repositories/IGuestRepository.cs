f92ed1d88533861585aaf7d2bf2d0e793573021c
Kim Andersson <Kimpabooy@live.com>
2025-05-27 09:13:02 +0200

    added GuestRepository and IGuestRepository

---
+++ Gotlandsrussen/Repositories/IGuestRepository.cs
@@ -0,0 +1,12 @@
+﻿using Gotlandsrussen.Models;
+
+namespace Gotlandsrussen.Repositories
+{
+    public interface IGuestRepository
+    {
+        Task<Guest?> GetById(int id);
+        Task<ICollection<Guest>> GetAllGuests();
+        Task<Guest> AddGuest(Guest guest);
+        Task<Guest> UpdateGuest(Guest guest);
+    }
+}
