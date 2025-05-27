7962204cf108996c12b81c3cf995d541a96460e3
Kim Andersson <Kimpabooy@live.com>
2025-05-27 08:27:02 +0200

    created CreateGuestRequestDto

---
+++ Gotlandsrussen/Models/DTOs/CreateGuestRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Gotlandsrussen.Models.DTOs
+{
+    public class CreateGuestRequestDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
