161bc95737d3b85e0f1cf5af459653bda2b4efe6
theMargarita <margotvoistinova@hotmail.se>
2025-05-28 14:20:59 +0200

    contructor - crated WITH GROUP

--- HotelDbTests/TestSetup/MockDataSetup.cs
+++ HotelDbTests/TestSetup/MockDataSetup.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Gotlandsrussen.Models;
+﻿using Gotlandsrussen.Models;
 using Gotlandsrussen.Repositories;
-using Moq;
-using Moq.Protected;
 
 namespace HotelGotlandsrussenTESTS.TestSetup
 {
