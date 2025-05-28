23b7409d90aa188c637a34f408347655afbdb0b9
theMargarita <margotvoistinova@hotmail.se>
2025-05-28 14:29:39 +0200

    added cooments

--- Gotlandsrussen/Repositories/IGuestRepository.cs
+++ Gotlandsrussen/Repositories/IGuestRepository.cs
@@ -4,9 +4,9 @@ namespace Gotlandsrussen.Repositories
 {
     public interface IGuestRepository
     {
-        Task<Guest?> GetById(int id);
-        Task<ICollection<Guest>> GetAllGuests();
-        Task<Guest> AddGuest(Guest guest);
+        Task<Guest?> GetById(int id); 
+        Task<ICollection<Guest>> GetAllGuests(); //Florent 
+        Task<Guest> AddGuest(Guest guest); //MArgo
         Task<Guest> UpdateGuest(Guest guest);
     }
 }
