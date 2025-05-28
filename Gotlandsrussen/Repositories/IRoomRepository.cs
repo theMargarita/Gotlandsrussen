f93f32aba4aa85d17094a251e83bfba31e495004
theMargarita <margotvoistinova@hotmail.se>
2025-05-28 14:29:45 +0200

    added cooments - moved a method to roomrepo

--- Gotlandsrussen/Repositories/IRoomRepository.cs
+++ Gotlandsrussen/Repositories/IRoomRepository.cs
@@ -4,6 +4,10 @@ namespace Gotlandsrussen.Repositories
 {
     public interface IRoomRepository
     {
-        public Task<ICollection<RoomDto>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children);
+        public Task<ICollection<RoomDto>> GetAvailableRoomByDateAndGuests(DateOnly fromDate, DateOnly toDate, int adults, int children); //lina
+
+
+        public Task<ICollection<RoomDto>> GetAvailableRoomsAsync(DateOnly startDate, DateOnly endDate); //kim
+
     }
 }
