3653e6200ce0da22f5165434c6d4fbb6e302c12b
theMargarita <margotvoistinova@hotmail.se>
2025-05-28 14:29:50 +0200

    added cooments - moved a method to roomrepo

--- Gotlandsrussen/Repositories/RoomRepository.cs
+++ Gotlandsrussen/Repositories/RoomRepository.cs
@@ -33,5 +33,34 @@ namespace Gotlandsrussen.Repositories
             return availableRooms;
         }
 
+
+        public async Task<ICollection<RoomDto>> GetAvailableRoomsAsync(DateOnly startDate, DateOnly endDate)
+        {
+            var bookedRoomIds = await _context.BookingRooms
+                .Include(br => br.Booking)
+                .Where(br =>
+                    !br.Booking.IsCancelled &&
+                    (startDate < br.Booking.ToDate &&
+                     endDate > br.Booking.FromDate))
+                .Select(br => br.RoomId)
+                .Distinct()
+                .ToListAsync();
+
+            var availableRooms = await _context.Rooms
+                .Include(r => r.RoomType)
+                .Where(r => !bookedRoomIds.Contains(r.Id))
+                .Select(r => new RoomDto
+                {
+                    Id = r.Id,
+                    RoomName = r.Name,
+                    RoomTypeName = r.RoomType.Name,
+                    NumberOfBeds = r.RoomType.NumberOfBeds,
+                    PricePerNight = r.RoomType.PricePerNight
+                })
+                .ToListAsync();
+
+            return availableRooms;
+        }
+
     }
 }
