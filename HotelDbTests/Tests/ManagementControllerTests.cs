184b0f819e4c27b67ac3a445eebefc97f3eea85c
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-30 15:41:03 +0200

    Added tests for GetAllFutureBookings

--- HotelDbTests/Tests/ManagementControllerTests.cs
+++ HotelDbTests/Tests/ManagementControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using Gotlandsrussen.Controllers;
 using Gotlandsrussen.Models.DTOs;
 using Gotlandsrussen.Repositories;
+using HotelGotlandsrussenTESTS.TestSetup;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -8,38 +10,72 @@ namespace HotelGotlandsrussenTESTS.Tests
 {
     [TestClass]
     public class ManagementControllerTests
-    {
-        [TestMethod]
-        
-        public async Task GetAllFutureBookings_ReturnsOkWithBookings()
-        {
-            //Arrange
-            var mockBookingRepo = new Mock<IBookingRepository>();
-            var mockRoomRepo = new Mock<IRoomRepository>();
-
-            var controller = new ManagementController(mockBookingRepo.Object, mockRoomRepo.Object);
-
-
-
-        }
-        
+    {   
         private Mock<IBookingRepository>? _mockBookingRepository;
         private Mock<IRoomRepository>? _mockRoomRepository;
+        private ManagementController _controller;
 
         [TestInitialize]
         public void Setup()
         {
             _mockBookingRepository = new Mock<IBookingRepository>();
             _mockRoomRepository = new Mock<IRoomRepository>();
-            ManagementController _controller = new ManagementController(_mockBookingRepository.Object, _mockRoomRepository.Object);
+            _controller = new ManagementController(_mockBookingRepository.Object, _mockRoomRepository.Object);
         }
 
-        [HttpGet("GetAllFutureBookings")] // lina
-        public async Task<ActionResult<ICollection<BookingDto>>> GetAllFutureBookings()
+        // Börja test här
+
+        [TestMethod]
+        public async Task GetAllFutureBookings_ReturnsOkWithBookings()
         {
-            return Ok(await _bookingRepository.GetAllFutureBookings());
+            //Arrange
+            var bookings = MockDataSetup.GetBookingDtos();
+
+            _mockBookingRepository.Setup(repo => repo.GetAllFutureBookings())
+                .ReturnsAsync(bookings);
+
+            //Act
+            var result = await _controller.GetAllFutureBookings();
+
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+
+            var returnedBookings = okResult.Value as ICollection<BookingDto>;
+            Assert.IsNotNull(returnedBookings);
+            Assert.AreEqual(3, returnedBookings.Count);
+
+            var bookingList = returnedBookings.ToList();
+            Assert.AreEqual(1, bookingList[0].Id);
+            Assert.AreEqual("Andersson, Alice", bookingList[0].GuestName);
+            Assert.AreEqual(new DateOnly(2025, 6, 10), bookingList[0].BookedFromDate);
+
+            _mockBookingRepository.Verify(repo => repo.GetAllFutureBookings(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetAllFutureBookings_ReturnsOkWithEmptyList()
+        {
+            //Arrange
+            var emptyBookingList = new List<BookingDto>();
+
+            _mockBookingRepository.Setup(repo => repo.GetAllFutureBookings())
+                .ReturnsAsync(emptyBookingList);
+
+            //Act
+            var result = await _controller.GetAllFutureBookings();
+
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+
+            var returnedBookings = okResult.Value as ICollection<BookingDto>;
+            Assert.IsNotNull(returnedBookings);
+            Assert.AreEqual(0, returnedBookings.Count);
+
+            _mockBookingRepository.Verify(repo => repo.GetAllFutureBookings(), Times.Once);
         }
     }
-        // Börja test här
-    } 
 }
