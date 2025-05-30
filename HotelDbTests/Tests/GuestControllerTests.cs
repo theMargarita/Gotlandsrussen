59c053ec116e8a2f94c8fdc1f89d34ea9027e5c3
Kim Andersson <Kimpabooy@live.com>
2025-05-30 13:04:48 +0200

    testclasses inits

--- HotelDbTests/Tests/GuestControllerTests.cs
+++ HotelDbTests/Tests/GuestControllerTests.cs
@@ -1,7 +1,29 @@
-﻿namespace HotelGotlandsrussenTESTS.Tests
+﻿using Gotlandsrussen.Controllers;
+using Gotlandsrussen.Models.DTOs;
+using Gotlandsrussen.Repositories;
+using HotelGotlandsrussenTESTS.TestSetup;
+using Moq;
+
+namespace HotelGotlandsrussenTESTS.Tests
 {
+    
+
     [TestClass]
     public class GuestControllerTest
     {
+        private Mock<IBookingRepository>? _mockBookingRepository;
+        private Mock<IRoomRepository>? _mockRoomRepository;
+        private Mock<IGuestRepository>? _mockGuestRepository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockBookingRepository = new Mock<IBookingRepository>();
+            _mockGuestRepository = new Mock<IGuestRepository>();
+            _mockRoomRepository = new Mock<IRoomRepository>();
+            GuestController _controller = new GuestController(_mockBookingRepository.Object, _mockGuestRepository.Object, _mockRoomRepository.Object);
+        }
+
+        // Börja test här
     }
 }
