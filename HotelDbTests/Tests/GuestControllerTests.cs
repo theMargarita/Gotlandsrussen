83a61066da18f762ec4aadb853db87f1168caf4a
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-31 22:52:23 +0200

    Testmethod startdatebeforetoday

--- HotelDbTests/Tests/GuestControllerTests.cs
+++ HotelDbTests/Tests/GuestControllerTests.cs
@@ -2,7 +2,9 @@
 using Gotlandsrussen.Models.DTOs;
 using Gotlandsrussen.Repositories;
 using HotelGotlandsrussenTESTS.TestSetup;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Collections;
 
 namespace HotelGotlandsrussenTESTS.Tests
 {
@@ -14,6 +16,7 @@ namespace HotelGotlandsrussenTESTS.Tests
         private Mock<IBookingRepository>? _mockBookingRepository;
         private Mock<IRoomRepository>? _mockRoomRepository;
         private Mock<IGuestRepository>? _mockGuestRepository;
+        private GuestController _controller;
 
         [TestInitialize]
         public void Setup()
@@ -21,9 +24,41 @@ namespace HotelGotlandsrussenTESTS.Tests
             _mockBookingRepository = new Mock<IBookingRepository>();
             _mockGuestRepository = new Mock<IGuestRepository>();
             _mockRoomRepository = new Mock<IRoomRepository>();
-            GuestController _controller = new GuestController(_mockBookingRepository.Object, _mockGuestRepository.Object, _mockRoomRepository.Object);
+            _controller = new GuestController(_mockBookingRepository.Object, _mockGuestRepository.Object, _mockRoomRepository.Object);
         }
 
         // Börja test här
+
+        [TestMethod]
+        public async Task GetAvailableRooms_StartDateBeforeToday_ReturnsBadRequest()
+        {
+            // Arrange
+            var startDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-2));
+            var endDate = DateOnly.FromDateTime(DateTime.Today.AddDays(2));
+        
+            // Act
+            var result = await _controller.GetAvailableRooms(startDate, endDate);
+        
+            // Assert
+            var badRequest = result as BadRequestObjectResult;
+            Assert.IsNotNull(badRequest);
+            Assert.AreEqual(400, badRequest.StatusCode);
+        }
+        
+        
+
+
+
+        //public async Task<IActionResult> GetAvailableRooms([FromQuery] DateOnly startDate, [FromQuery] DateOnly endDate)
+        //{
+        //    if (startDate < DateOnly.FromDateTime(DateTime.Today))
+        //        return BadRequest("Startdatum har redan passerat.");
+
+        //    if (startDate >= endDate)
+        //        return BadRequest("Startdatum måste vara före slutdatum.");
+
+        //    var rooms = await _roomRepository.GetAvailableRoomsAsync(startDate, endDate);
+        //    return Ok(rooms);
+        //}
     }
 }
