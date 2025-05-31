bb8538ce2676ce84906e3ae04c30acbaaa4125c9
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-31 23:40:39 +0200

    New class for RoomRepositoryTests

---
+++ HotelDbTests/Tests/RoomRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using Gotlandsrussen.Data;
+using Gotlandsrussen.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGotlandsrussenTESTS.Tests
+{
+    [TestClass]
+    public class RoomRepositoryTests
+    {
+        private RoomRepository _repository;
+        private HotelDbContext _context;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<HotelDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Unik databas för varje test
+                .Options;
+
+            _context = new HotelDbContext(options);
+            _context.Database.EnsureCreated();
+
+            _repository = new RoomRepository(_context);
+        }
+    }
+}
