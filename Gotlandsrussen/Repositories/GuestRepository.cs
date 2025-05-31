35a3ecf94bd512214594dcc70af30bbd6c3feb03
LinaOlandersson <olanderssonlina@gmail.com>
2025-05-31 23:44:19 +0200

    Added names to repository methods

--- Gotlandsrussen/Repositories/GuestRepository.cs
+++ Gotlandsrussen/Repositories/GuestRepository.cs
@@ -11,25 +11,25 @@ namespace Gotlandsrussen.Repositories
             _context = context;
         }
 
-        public async Task<Guest> AddGuest(Guest guest)
+        public async Task<Guest> AddGuest(Guest guest)  //Kim
         {
             var addedGuest = _context.Guests.Add(guest);
             _context.SaveChanges();
             return addedGuest.Entity;
         }
 
-        public async Task<ICollection<Guest>> GetAllGuests()
+        public async Task<ICollection<Guest>> GetAllGuests()   //Lina
         {
             var getAllGuests = _context.Guests.ToList();
             return getAllGuests;
         }
 
-        public async Task<Guest?> GetById(int id)
+        public async Task<Guest?> GetById(int id)   //Margarita
         {
             throw new NotImplementedException();
         }
 
-        public async Task<Guest> UpdateGuest(Guest guest)
+        public async Task<Guest> UpdateGuest(Guest guest)    //Florent
         {
             throw new NotImplementedException();
         }
