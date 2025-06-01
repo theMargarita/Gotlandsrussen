9afb5d20e02e4b40c28e257c485ae3e4ea96b795
Kim Andersson <Kimpabooy@live.com>
2025-06-01 15:52:31 +0200

    commit to bring in updated main

--- HotelDbTests/Tests/GuestControllerTests.cs
+++ HotelDbTests/Tests/GuestControllerTests.cs
@@ -1,12 +1,12 @@
 ﻿using Gotlandsrussen.Controllers;
-using Gotlandsrussen.Models.DTOs;
 using Gotlandsrussen.Repositories;
 using HotelGotlandsrussenTESTS.TestSetup;
 using Moq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelGotlandsrussenTESTS.Tests
 {
-    
+
 
     [TestClass]
     public class GuestControllerTest
@@ -14,6 +14,7 @@ namespace HotelGotlandsrussenTESTS.Tests
         private Mock<IBookingRepository>? _mockBookingRepository;
         private Mock<IRoomRepository>? _mockRoomRepository;
         private Mock<IGuestRepository>? _mockGuestRepository;
+        private GuestController? _controller;
 
         [TestInitialize]
         public void Setup()
@@ -21,9 +22,38 @@ namespace HotelGotlandsrussenTESTS.Tests
             _mockBookingRepository = new Mock<IBookingRepository>();
             _mockGuestRepository = new Mock<IGuestRepository>();
             _mockRoomRepository = new Mock<IRoomRepository>();
-            GuestController _controller = new GuestController(_mockBookingRepository.Object, _mockGuestRepository.Object, _mockRoomRepository.Object);
+            _controller = new GuestController(_mockBookingRepository.Object, _mockGuestRepository.Object, _mockRoomRepository.Object);
+
         }
 
         // Börja test här
+
+        [TestMethod]
+        public async Task GetBookingById_ShouldReturnCorrectBooking_ReturnsTrue()
+        {
+            // Arrange
+            var mockData = MockDataSetup.GetBookings();
+            var mockDataObject = MockDataSetup.GetBookings()[0];
+
+            // Setup the mock repository to return the mock data when GetById is called
+            _mockBookingRepository.Setup(gb => gb.GetById(1))
+                .ReturnsAsync(mockDataObject);
+
+            // Act
+            var result = await _controller.GetBookingById(1);
+            var resultData = result.Value;
+
+            // Assert
+            //var okReult = result.Result as OkObjectResult;
+
+            Assert.IsNotNull(resultData, "Result is not OkObjectResult");
+            Assert.IsNotNull(result, "result är null");
+            Assert.IsNotNull(result.Value, "result.Value är null");
+            Assert.AreEqual(mockData[0].Id, resultData.Id);
+            //Assert.AreEqual(mockData[0].Id, result.Value.Id);
+
+        }
+
+
     }
 }
