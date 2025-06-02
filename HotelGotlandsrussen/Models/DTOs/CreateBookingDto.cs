c2514ff0425b912633aa230b9f7d15ea3108ab23
theMargarita <margotvoistinova@hotmail.se>
2025-06-02 23:07:42 +0200

    created for cratebooking

---
+++ HotelGotlandsrussen/Models/DTOs/CreateBookingDto.cs
@@ -0,0 +1,21 @@
+﻿using Gotlandsrussen.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGotlandsrussenLIBRARY.Models.DTOs
+{
+    public class CreateBookingDto
+    {
+        public int GuestId { get; set; }
+        public DateOnly FromDate { get; set; } // Booked room from this date
+        public DateOnly ToDate { get; set; } // Booked room To this date
+        public int NumberOfAdults { get; set; }
+        public int NumberOfChildren { get; set; }
+        public bool Breakfast { get; set; } = false; //not sure about this one
+        public List<RoomDto>? Rooms { get; set; }
+
+    }
+}
