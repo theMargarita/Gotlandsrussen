cb026fc1e831638c077d58cbe97a3280a37f778e
theMargarita <margotvoistinova@hotmail.se>
2025-06-02 23:08:37 +0200

    deleted some comments

--- HotelGotlandsrussen/Models/DTOs/CreateBookingDto.cs
+++ HotelGotlandsrussen/Models/DTOs/CreateBookingDto.cs
@@ -10,11 +10,11 @@ namespace HotelGotlandsrussenLIBRARY.Models.DTOs
     public class CreateBookingDto
     {
         public int GuestId { get; set; }
-        public DateOnly FromDate { get; set; } // Booked room from this date
-        public DateOnly ToDate { get; set; } // Booked room To this date
+        public DateOnly FromDate { get; set; } 
+        public DateOnly ToDate { get; set; } 
         public int NumberOfAdults { get; set; }
         public int NumberOfChildren { get; set; }
-        public bool Breakfast { get; set; } = false; //not sure about this one
+        public bool Breakfast { get; set; } = false; 
         public List<RoomDto>? Rooms { get; set; }
 
     }
