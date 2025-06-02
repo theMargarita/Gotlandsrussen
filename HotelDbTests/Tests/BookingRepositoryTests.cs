e9b4c7839f81ace9fc079696423b0bf95acfa223
LinaOlandersson <olanderssonlina@gmail.com>
2025-06-02 16:15:11 +0200

    testMethod for updating booking

--- HotelDbTests/Tests/BookingRepositoryTests.cs
+++ HotelDbTests/Tests/BookingRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using Gotlandsrussen.Data;
+using Gotlandsrussen.Models;
 using Gotlandsrussen.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -34,6 +35,31 @@ namespace HotelGotlandsrussenTESTS.Tests
         //Det är en kopia av databasen som endast ligger i minnet.
         //Varje nytt test ger en ny fräsch DbContext. Det sparas alltså inget mellan testerna.
 
+        [TestMethod]
+        public async Task Update_ChangingNumberOfAdults_UpdatesNumberOfAdultsInBooking()
+        {
+            //Arrange
+            var booking = new Booking { NumberOfAdults = 4 };
+            _context.Bookings.Add(booking);
+            await _context.SaveChangesAsync();
+            
+            var originalNumberOfAdults = booking.NumberOfAdults;
+            var bookingId = booking.Id;
 
+            var bookingToUpdate = await _context.Bookings.FindAsync(bookingId);
+            bookingToUpdate.NumberOfAdults = 2;
+
+            //Act
+            await _repository.Update(bookingToUpdate);
+
+            //Assert
+            var updatedBooking = await _context.Bookings.FindAsync(bookingId);
+
+            Assert.IsNotNull(updatedBooking);
+            Assert.AreNotEqual(originalNumberOfAdults, updatedBooking.NumberOfAdults);
+            Assert.AreEqual(2, updatedBooking.NumberOfAdults);
+        }
+
+       
     }
 }
