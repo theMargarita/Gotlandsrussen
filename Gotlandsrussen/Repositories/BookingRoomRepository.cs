a98b29b7bcf1e2af3059e7509f51e07565b27714
theMargarita <margotvoistinova@hotmail.se>
2025-06-04 14:44:04 +0200

    added bookingroom repo

---
+++ Gotlandsrussen/Repositories/BookingRoomRepository.cs
@@ -0,0 +1,21 @@
+﻿using Gotlandsrussen.Data;
+using Gotlandsrussen.Models;
+
+namespace GotlandsrussenAPI.Repositories
+{
+    public class BookingRoomRepository : IBookingRoomRepository
+    {
+        private readonly HotelDbContext _context;
+        public BookingRoomRepository(HotelDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<BookingRoom> AddBookingRooms(BookingRoom bookingRoom)
+        {
+            var booking = await _context.BookingRooms.AddAsync(bookingRoom);
+            await _context.SaveChangesAsync();
+
+            return booking.Entity;
+        }
+    }
+}
---
+++ Gotlandsrussen/Repositories/IBookingRoomRepository.cs
@@ -0,0 +1,9 @@
+﻿using Gotlandsrussen.Models;
+
+namespace GotlandsrussenAPI.Repositories
+{
+    public  interface IBookingRoomRepository
+    {
+        public Task<BookingRoom> AddBookingRooms(BookingRoom bookingRoom);
+    }
+}
