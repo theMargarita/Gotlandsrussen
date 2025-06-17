711ba956218cffece13693c33656f87117810af4
FlorentHajdari <florent.hajdari@hotmail.se>
2025-06-17 20:29:28 +0200

    Updated Database

---
+++ Gotlandsrussen/Migrations/20250617182025_AddIsCleanProperty.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GotlandsrussenAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCleanProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCleaned",
+                table: "Rooms",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "IsCleaned",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "IsCleaned",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "IsCleaned",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCleaned",
+                table: "Rooms");
+        }
+    }
+}
